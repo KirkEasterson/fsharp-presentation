19d5d771016a535fddb2ef18b8189296b738bb89
Kirk Easterson <kirk.easterson@gmail.com>
2023-06-06 09:39:07 +0200

    Set up directory layout and added basic print statements

---
+++ basics/csharp/Program.cs
@@ -0,0 +1,3 @@
+﻿string name = "Odo";
+int age = 18;
+System.Console.WriteLine($"{name} is {age} years old!");
---
+++ basics/fsharp/Program.fs
@@ -0,0 +1,3 @@
+﻿let name:string = "Odo"
+let age:int = 18
+printfn "%s is %d years old!" name age
