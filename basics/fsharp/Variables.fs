9e52fa28eb908315cdd7a41a6082cefe7e2d3821
Kirk Easterson <kirk.easterson@gmail.com>
2023-06-19 10:11:36 +0200

    Moved items in variables

--- basics/fsharp/Variables.fs
+++ basics/fsharp/Variables.fs
@@ -2,9 +2,21 @@ module Variables
 
 let run () =
 
+    printfn "Hello, World!"
+
+    // initializing variables
     let variable = "value"
     let mutable name: string = "Odo"
     let age: int = 18
+
+    // printing
     printfn "%s is %d years old!" name age
-    name <- "Laas" // assignment operator
-    // age <- 20000// not allowed, `age` is not mutable
+
+    // quiz: why isn't this allowed
+    // printfn name
+
+    // assigning to a mutable variable
+    name <- "Laas"
+
+    // immutable variables cannot be reassigned
+    // age <- 20000
