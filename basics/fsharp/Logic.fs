0cc2e3ea36be3a3d32394a5f4548de4315eba613
Kirk Easterson <kirk.easterson@gmail.com>
2023-06-20 10:56:51 +0200

    Added complex pattern matching to logic

--- basics/fsharp/Logic.fs
+++ basics/fsharp/Logic.fs
@@ -14,3 +14,11 @@ let run args =
 
     if bar = baz then
         printfn "bar equals baz"
+
+    // quiz: what will be printed?
+    match (foo, bar, baz) with
+    | (true, _, false) -> "foo is true, but baz is false"
+    | (true, false, _) -> "foo is true, but bar is false"
+    | (false, false, false) -> "everything is false"
+    | (_, _, _) -> "they all have some values"
+    |> printfn "%s"
